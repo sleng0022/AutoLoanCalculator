979d12e7a48974d1b45d57bb2c8b292d2788e0cd
leng.sinith001@gmail.com <sinithleng@Siniths-MacBook-Pro.local>
2017-10-14 21:22:39 -0500

    Add initial gui development work.

---
+++ test/unit_test_month_payment.java
@@ -0,0 +1,13 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class unit_test_month_payment {
+
+	@Test
+	final void test() 
+	{
+		fail("Not yet implemented"); // TODO
+	}
+
+}
